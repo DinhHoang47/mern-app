51f345ebe8f5a8608588d17fce1dc1505fdb4d02
DinhHoang47 <dinhhoang0407@gmail.com>
2023-07-05 11:52:41 +0700

    Implement Google sign-in button with GIS

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import { Container } from "@mui/material";
 
 import NavBar from "./components/Navbar/NavBar";
@@ -8,15 +9,23 @@ import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
-    <Router>
-      <Container style={{ marginBottom: "150px" }} maxWidth="lg">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </Container>
-    </Router>
+    <>
+      <Helmet>
+        <meta
+          http-equiv="Cross-Origin-Opener-Policy"
+          content="same-origin-allow-popups"
+        />
+      </Helmet>
+      <Router>
+        <Container style={{ marginBottom: "150px" }} maxWidth="lg">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </Container>
+      </Router>
+    </>
   );
 };
 
--- clients/src/reducers/auth.js
+++ clients/src/reducers/auth.js
@@ -2,8 +2,9 @@ import { AUTH, LOGOUT } from "../constants/actionTypes";
 export default (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
       console.log(action.payload);
-      return state;
+      return { ...state, authData: action.payload };
 
     default:
       return state;
